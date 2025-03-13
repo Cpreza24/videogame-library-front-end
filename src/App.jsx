85fb7387f1a6b3e86a8615b2e6d43afb60df2d30
Cesar Preza <cpreza93@yahoo.com>
2025-03-12 21:21:08 -0700

    adds links for console list and renders consoles to the browser via links

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
-import { useContext } from 'react';
-import { Routes, Route } from 'react-router';
+import { useContext, useState, useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
-
+import ConsoleList from './components/ConsoleList/ConsoleList';
+import * as consoleService from './services/consoleService';
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+  const navigate = useNavigate();
   const { user } = useContext(UserContext);
-  
+  const [consoles, setConsoles] = useState([]);
+
+  useEffect(() => {
+    const fetchAllConsoles = async () => {
+      const consoleData = await consoleService.index();
+      setConsoles(consoleData);
+    };
+    if (user) fetchAllConsoles();
+  }, [user]);
+
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <Routes>
         <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
+        {user ? (
+          <>
+            <Route
+              path='/consoles'
+              element={<ConsoleList consoles={consoles} />}
+            />
+          </>
+        ) : (
+          <>
+            <Route path='/sign-up' element={<SignUpForm />} />
+            <Route path='/sign-in' element={<SignInForm />} />
+          </>
+        )}
       </Routes>
     </>
   );
---
+++ src/components/ConsoleList/ConsoleList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+function ConsoleList({ consoles }) {
+  return (
+    <main>
+      <div>
+        <h1>Consoles List</h1>
+      </div>
+      <div>
+        {consoles.map((console) => (
+          <Link key={console._id}>
+            <h2>{console.name}</h2>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
+
+export default ConsoleList;
